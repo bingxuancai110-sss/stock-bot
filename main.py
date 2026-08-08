0fe7860881b89ad8733c9c094e96c6cc289d21c1
你的名字或英文代號 <Email>
2026-08-08 23:31:01 +0800

    add line bot code

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask 
+from linebot.exceptions import InvalidSignatureError 
+from linebot.models import MessageEvent, TextSendMessage, TextMessage 
+ 
 app = Flask(__name__) 
  
-@app.route('/') 
-def home(): 
-    return "Stock Bot is running!" 
+line_bot_api = LineBotApi('你的Channel Access Token') 
+handler = WebhookHandler('你的Channel Secret') 
+ 
+@app.route("/callback", methods=['POST']) 
+def callback(): 
+    signature = request.headers['X-Line-Signature'] 
+    body = request.get_data(as_text=True) 
+    try: 
+        handler.handle(body, signature) 
+    except InvalidSignatureError: 
+        abort(400) 
+    return 'OK' 
+ 
+@handler.add(MessageEvent, message=TextMessage) 
+def handle_message(event): 
+    user_message = event.message.text 
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"你說了: {user_message}")) 
  
 if __name__ == '__main__': 
     app.run() 
